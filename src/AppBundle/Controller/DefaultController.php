a04b1e17af634c13442321a4c1de42b654ff50df
lapiscine <aurelie.fougere@lapiscine.pro>
2018-11-23 11:37:18 +0100

    v1.0 Comit du vendredi 23/11

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,21 +1,128 @@
 <?php
-
+/**
+ * Le namespace est le "chemin" permettant de cibler la classe et l'identifier
+ */
 namespace AppBundle\Controller;
 
+//C'est le namespace des classes qui vont etre utiliéses dans cette class DefaultController
+use AppBundle\Entity\Autor;
+use AppBundle\Entity\Book;
+use http\Exception\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+//Déclaration de la class qui hérite de la classe Controller
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/book", name="book")
+     *Fonctionnalité (composant le Router de Symfony) permettant d'afficher la page BookTest
+     * Le router lie un chemin à une action de controleur. L'action de controleur
+     * affiche un fichier twig (qui lui meme appelle la base) = réponse HTTP envoyée au client
+     * Cette méthode va afficher un article (1) de l'Entité "Book" (qui est le miroir de la table Book dans la Bdd)
+     * C'est un affiche brut avec le var_dump
+     *
      */
-    public function indexAction(Request $request)
+    public function BookTest()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $repository = $this->getDoctrine()->getRepository(Book::class);
+
+        $Livre = $repository->find(1);
+
+        var_dump($Livre); die;
+    }
+
+    /**
+     * @Route("/authors", name="authors")
+     * Cette méthode appel le framework Doctrine et crée une instance de Doctrine (getDoctrine, et héritage Controller on accède à Doctrine),
+     * puis un appel au repository (méthode de doctrine),
+     * qui va chercher / récupérer toute l'Entité Autor. Tout ceci sera stocké dans une variable.
+     * Ensuite on fait appel à une méthode de repository qui va demander de récupérer tout dans l'entite.
+     * Le render permet d'afficher le resultat dans un twig.
+     * $Auteur contient une liste d'objet, c'est une instance de l'entité Autor. Pour chaque entrée on va avoir une entrée.
+     */
+    public function ListAuthorsAction()
+    {
+        $repository = $this->getDoctrine()->getRepository(Autor::class);
+
+        $Auteur = $repository->findAll();
+
+        return $this->render("@App/author.html.twig",
+            [
+                'tableaux' => $Auteur
+            ]);
+
+    }
+
+    /**
+     * @Route("/author/{id}", name="author")
+     * On a créé un lien sur le nom des auteurs, renvoyant vers cette méthode, qui va afficher un auteur sur une nouvelle page
+     */
+    public function InfoAuthorAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Autor::class);
+
+        $Auteur = $repository->find($id);
+
+        return $this->render('@App/onlyauthor.html.twig',
+        [
+            'author' => $Auteur
         ]);
     }
+
+    /**
+     * @Route("/livresbygenre/{genre}", name="livres_by_Genre")
+     * On crée une méthode qui sera déclarée dans le Repository, dans BookRepository
+     */
+    public function BooksGenreAction($genre)
+    {
+        $repository = $this->getDoctrine()->getRepository(Book::class);
+
+        $Auteurs = $repository->getBooksByGenre($genre);
+
+        return $this->render('@App/genre.html.twig',
+            [
+               'auteurs' => $Auteurs
+            ]);
+    }
+
+    /**
+     * @Route("/paysauteur/{id}", name="pays_auteur")
+     * Je récupère une instance de Doctrine qui appelle une instense de repository
+     */
+    public function PaysAuthorsAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Autor::class);
+
+        $Land = $repository->getBooksByPays($id);
+
+        return $this->render('@App/pays.html.twig',
+        [
+            'listes' => $Land
+        ]);
+    }
+
+
+    /**
+     * @Route("/recherche", name="recherche")
+     *
+     */
+    public function FindWordAction(Request $request)
+    {
+        $word = $request->query->get('recherche');
+
+        $repository = $this->getDoctrine()->getRepository(Autor::class);
+
+        $biographyes = $repository->getFindWord($word);
+
+        return $this->render('@App/recherche.html.twig',
+        [
+            'biographyes' => $biographyes
+        ]);
+    }
+
+
+
 }
--- src/AppBundle/Entity/Autor.php
+++ src/AppBundle/Entity/Autor.php
@@ -8,19 +8,144 @@
 
 namespace AppBundle\Entity;
 
+//On cherche l'adresse de la classe Mapping, on importe le namespace pour utiliser la classe Mapping qui porte l'alias ORM
 use Doctrine\ORM\Mapping as ORM;
 
+// (@ORM => La class Autor sera défini comme une classe)
+//(@ORM\Table => Défini la Table)
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AutorRepository")
  * @ORM\Table(name="autor")
  */
 class Autor
 {
- /**
-  * @ORM\Column(type="integer")
-  * @ORM\Id
-  * @ORM\GeneratedValue(strategy="AUTO")
-  */
- private $id;
+
+     //Doctrine positionne l'Id en required
+     //Id est une clé primaire
+    //@ORM\GeneratedValue, n'est mise qu'au niveau de la clé primaire, sur l'Id
+     /**
+      * @ORM\Column(type="integer")
+      * @ORM\Id
+      * @ORM\GeneratedValue(strategy="AUTO")
+      */
+     private $id;
+
+     /**
+      *
+      * @ORM\Column(type="datetime", name="borned_at", nullable=true)
+      */
+     private $bornedat;
+
+    /**
+     * @ORM\Column(type="datetime", name="dead_at")
+     */
+    private $deadat;
+
+
+     /**
+      * @ORM\Column(type="text")
+      */
+     private $biography;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $nom;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    
+
+    /**
+     * @return mixed
+     */
+    public function getBornedat()
+    {
+        return $this->bornedat;
+    }
+
+    /**
+     * @param mixed $bornedat
+     */
+    public function setBornedat($bornedat)
+    {
+        $this->bornedat = $bornedat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeadat()
+    {
+        return $this->deadat;
+    }
+
+    /**
+     * @param mixed $deadat
+     */
+    public function setDeadat($deadat)
+    {
+        $this->deadat = $deadat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+
+    /**
+     * @param mixed $biography
+     */
+    public function setBiography($biography)
+    {
+        $this->biography = $biography;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $pays;
+
+    /**
+     * @return mixed
+     */
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    /**
+     * @param mixed $pays
+     */
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
+    }
+
 
 }
